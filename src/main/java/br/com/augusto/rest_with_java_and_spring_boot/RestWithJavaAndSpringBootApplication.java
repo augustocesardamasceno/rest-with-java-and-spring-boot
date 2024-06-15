8400310883a6c08c1ca2df2dcd6e41260d110fd5
augustocesardamasceno <augustoccmd@hotmail.com>
2024-06-15 10:54:02 -0300

    First commit

---
+++ src/main/java/br/com/augusto/rest_with_java_and_spring_boot/RestWithJavaAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.augusto.rest_with_java_and_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithJavaAndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithJavaAndSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/augusto/rest_with_java_and_spring_boot/RestWithJavaAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.augusto.rest_with_java_and_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithJavaAndSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
